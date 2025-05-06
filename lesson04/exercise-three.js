b7880e804ec6fca42141686b726faef02f5e020c
gchang12 <gc8@hawaii.edu>
2025-05-06 07:10:37 -0700

    added exercise3

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,17 @@
+const depressionSymptoms = [
+  "low energy",
+  "rumination",
+  "thought-blocking",
+  "hopelessness",
+  "existential despair",
+  "loss of purpose",
+  "anhedonia",
+  "envy for those more fortunate",
+  "numbness",
+  "chronic fatigue and drowsiness",
+  "misophonia wrt people talking",
+];
+
+for (let i=0; i < depressionSymptoms.length; i++) {
+  console.log(`${i+1},${depressionSymptoms[i]}`);
+}
